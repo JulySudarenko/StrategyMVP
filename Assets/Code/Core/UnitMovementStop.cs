d32e486ab106d0e1667b23b02f5d5d32cd9e874b
July Sudarenko <july@sudarenko.ru>
2021-10-05 15:25:50 +0300

    Unit animated

---
+++ Assets/Code/Core/UnitMovementStop.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovementStop : MonoBehaviour, IAwaitable<AsyncExtensions.Void>
+{
+    public class StopAwaiter : IAwaiter<AsyncExtensions.Void>
+    {
+        private readonly UnitMovementStop _unitMovementStop;
+        private Action _continuation;
+        private bool _isCompleted;
+
+        public StopAwaiter(UnitMovementStop unitMovementStop)
+        {
+            _unitMovementStop = unitMovementStop;
+            _unitMovementStop.OnStop += OnStop;
+        }
+
+        private void OnStop()
+        {
+            _unitMovementStop.OnStop -= OnStop;
+            _isCompleted = true;
+            _continuation?.Invoke();
+        }
+
+        public void OnCompleted(Action continuation)
+        {
+            if (_isCompleted)
+            {
+                continuation?.Invoke();
+            }
+            else
+            {
+                _continuation = continuation;
+            }
+        }
+
+        public bool IsCompleted => _isCompleted;
+        public AsyncExtensions.Void GetResult() => new AsyncExtensions.Void();
+    }
+
+    public event Action OnStop;
+
+    [SerializeField] private NavMeshAgent _agent;
+
+    public void Update()
+    {
+        if (!_agent.pathPending)
+        {
+            if (_agent.remainingDistance <= _agent.stoppingDistance)
+            {
+                if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
+                {
+                    OnStop?.Invoke();
+                }
+            }
+        }
+    }
+
+    public IAwaiter<AsyncExtensions.Void> GetAwaiter() => new StopAwaiter(this);
+}
